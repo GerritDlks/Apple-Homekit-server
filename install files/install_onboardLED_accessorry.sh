c792aa3e0c84ac44932193001f1a222d0431689f
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2017-06-14 22:38:08 +0200

    Updated URLs

--- install files/install_onboardLED_accessorry.sh
+++ install files/install_onboardLED_accessorry.sh
@@ -3,9 +3,9 @@
 # Install the accessory
 cd /home/pi/HAP-NodeJS/accessories/
 sudo rm -rf Light_accessory.js
-sudo wget https://raw.githubusercontent.com/Kevin-De-Koninck/Apple-Home-on-RPi/master/Accessories/Light_accessory-OnBoardLED.js
+sudo wget https://raw.githubusercontent.com/Kevin-De-Koninck/Apple-Homekit-and-PiHole-server/master/accessories/Light_OnBoardLED_accessory.js
 
 # Install the script
 sudo mkdir /home/pi/HAP-NodeJS/python
 cd /home/pi/HAP-NodeJS/python
-sudo wget https://raw.githubusercontent.com/Kevin-De-Koninck/Apple-Home-on-RPi/master/Python%20scripts/onboard-LED.py
+sudo wget https://raw.githubusercontent.com/Kevin-De-Koninck/Apple-Homekit-and-PiHole-server/master/python%20scripts/onboard-LED.py
